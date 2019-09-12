5aa69050ffa180b6a2b9b0f387f49a9d665736c7
John Walsh <lt00036644@students.ittralee.local>
2019-09-12 12:14:07 +0100

    Initial commit

---
+++ src/paddyspackage/BigBensHello.java
@@ -0,0 +1,10 @@
+package paddyspackage;
+
+import javax.swing.*;
+
+public class BigBensHello {
+    public static void main(String[] args) {
+       String name = JOptionPane.showInputDialog("enter Name");
+    JOptionPane.showMessageDialog(null,"hello "+name);
+    }
+}
---
+++ src/paddyspackage/BigHello.java
@@ -0,0 +1,7 @@
+package paddyspackage;
+
+public class BigHello {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
